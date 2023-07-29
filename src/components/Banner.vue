7647993606dac7778fc10010b3347cb877096fba
catabsilva <work@catarinasilva.com>
2023-07-29 15:02:26 +0200

    first commit

---
+++ src/components/Banner.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="main-banner">
+        <h1>Make your Burger</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Banner'
+}
+</script>
+
+<style scoped>
+    #main-banner {
+        background-image: url('/public/img/burger.jpg');
+        background-position: 100%;  
+        background-size: cover;
+        height: 500px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+
+    }
+
+
+    #main-banner h1 {
+        color: #FFF;
+        text-align: center;
+        font-size: 40px;
+        background-color: #22222262 ;
+        padding: 20px 40px;
+    }
+</style>
